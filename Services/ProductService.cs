54bafadb93652593665210ee938cc3713c2eb009
renatolacerda <renatolacerda@matur.com.br>
2020-06-24 13:57:46 -0300

    atuaçlização

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -20,6 +20,21 @@ namespace Weba2mvc.Services
         {
             return _context.Product.ToList();
         }
+        public void Insert(Product obj) 
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+        public Product FindById(int id) 
+        {
+            return _context.Product.FirstOrDefault(obj => obj.Id == id);
+        }
+        public void Remove(int id)
+        {
+            Product obj = _context.Product.Find(id);
+            _context.Product.Remove(obj);
+            _context.SaveChanges();
+        }
 
     }
 }
