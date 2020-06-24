9e4f84d7a021da113ed6d381531069d04be69809
renatolacerda <renatolacerda@matur.com.br>
2020-06-24 13:57:09 -0300

    atualiza

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -20,5 +20,41 @@ namespace Weba2mvc.Controllers
             var list = _productService.FindAll();
             return View(list);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product obj) 
+        {
+            _productService.Insert(obj);
+            return RedirectToAction(nameof(Index));
+        
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Product obj = _productService.FindById(id.Value);
+            if (obj == null) 
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _productService.Remove(id);
+            return RedirectToAction(nameof(Index));            
+        }
+        
     }
 }
